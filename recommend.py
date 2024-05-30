65e6e4a9e4a7563c7096767b1c2798ed326f4ae8
HoangAkyc29 <nguyenhoang29minh03@gmail.com>
2024-05-30 19:03:31 +0700

    "update code"

--- recommend.py
+++ recommend.py
@@ -4,6 +4,7 @@ import re
 import unicodedata
 from Model_Here.keyphrase import TextEntities_Score
 from Model_Here.synonyms_prediction import re_cluster
+from Model_Here.score_extract import annotate_text
 from Crawler_Here.translate_tool import final_translate_text
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -53,9 +54,11 @@ def get_new_contentbase_df():
 
 def recommend_system(text):
     # print("Hi")
-    dicts_sentiment, dicts_adj_feature, list_noun_feature, list_proper_noun_feature = TextEntities_Score(text,True)
+    dicts_sentiment, _, _, _ = TextEntities_Score(text,True)
+    list_proper_noun_feature = [word['wordForm'] for word in annotate_text(text) if word['nerLabel'] in ['B-LOC', 'B-PER']]
     # new_contentbase_df = get_new_contentbase_df()
-    # print(list_proper_noun_feature)
+    print(dicts_sentiment)
+    print(list_proper_noun_feature)
     conduct_content_base(dicts_sentiment, list_proper_noun_feature)
     
     return conduct_content_base(dicts_sentiment, list_proper_noun_feature)
@@ -93,24 +96,26 @@ def conduct_content_base(dicts_sentiment, list_proper_noun_feature):
         if proper_noun in column_names:
             comment_cluster_list.append(proper_noun)
     
-    vector = [0] * len(column_names)
-    # Duyệt qua từng tên cột và kiểm tra
-    for i, col in enumerate(column_names):
-        if col in comment_cluster_list:
-            vector[i] = 2
+    filtered_df = content_base_df[comment_cluster_list]
+
+    vector = [2] * len(filtered_df.columns)
+    # # Duyệt qua từng tên cột và kiểm tra
+    # for i, col in enumerate(column_names):
+    #     if col in comment_cluster_list:
+    #         vector[i] = 1
 
     # Tạo DataFrame từ lịch sử hoạt động của User1
-    user_history_df = pd.DataFrame([vector], columns=content_base_df.columns)
+    user_history_df = pd.DataFrame([vector], columns=filtered_df.columns)
     # Tính toán độ tương tự cosine giữa lịch sử hoạt động của người dùng và các địa danh
-    user_similarity = cosine_similarity(user_history_df, content_base_df)
+    user_similarity = cosine_similarity(user_history_df, filtered_df)
 
     # Chuyển ma trận độ tương tự thành DataFrame để dễ xử lý
-    user_similarity_df = pd.DataFrame(user_similarity, index=user_history_df.index, columns=content_base_df.index)
+    user_similarity_df = pd.DataFrame(user_similarity, index=user_history_df.index, columns=filtered_df.index)
 
     # Sắp xếp ma trận user_similarity_df theo thứ tự giảm dần của các giá trị tương tự
     sorted_user_similarity_df = user_similarity_df.apply(lambda row: row.sort_values(ascending=False), axis=1)
 
     return sorted_user_similarity_df
 
-# text = "Hãy giới thiệu cho tôi địa điểm ăn chơi tại Đà Nẵng."
-# print(recommend_system(text))
+text = "giới thiệu cho tôi 1 chỗ nhà hàng đồ ăn tại Đà Nẵng."
+print(recommend_system(text))
