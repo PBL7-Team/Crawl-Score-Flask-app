6f492484f8e1ed983ade3190b1cf9eb24c02e16c
vinhnado <vinh.nd0231@gmail.com>
2024-05-30 11:32:39 +0700

    update connect db

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from service_crawl import get_all_json_data, update_csv_with_json_data, start_cr
 from service_model import sentiment_analysis_all, fully_updated_sentiment_csv, export_synonyms_clusters
 from dotenv import load_dotenv
 from functools import wraps
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 load_dotenv()
@@ -15,6 +17,13 @@ load_dotenv()
 # Lấy các giá trị của các biến môi trường
 API_KEY_1 = os.getenv("API_KEY_1")
 API_KEY_2 = os.getenv("API_KEY_2")
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456789'
+app.config['MYSQL_DB'] = 'pbl7'
+ 
+mysql = MySQL(app)
 
 crawl_thread = None
 crawl_thread_mode_2 = None
@@ -163,6 +172,25 @@ def recommend():
     return jsonify({
         "ok": msg,
     }), 200
+    
+    
+@app.route('/update-db', methods = ['GET'])
+@require_api_key
+def update_db():
+    cursor = mysql.connection.cursor()
+    try:
+        cursor.execute("SELECT 1")
+        
+        mysql.connection.commit()
+        cursor.close()
+    except Exception as e:
+        print("Connection Error:", str(e))
+        cursor.close()
+        return "DB Connection Error", 500
+    return jsonify({
+        "ok": "Ok",
+    }),
+    
 
 # bước 1: Crawl data bằng start-crawl
 # bước 2: Thực hiện sentiment caculate (để cập nhật score vào mỗi file json), sử dụng model entity extraction + sentiment analysis
@@ -172,4 +200,4 @@ def recommend():
 
 # bước 1.2: Crawl data bằng mode 2 (để cập nhật dữ liệu review từ địa điểm cũ)
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', port=8080)
